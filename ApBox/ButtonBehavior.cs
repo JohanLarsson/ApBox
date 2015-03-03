fff078fa811df78ec847cb87d3adc881ed11ae89
Johan Larsson <johan_gavle1@hotmail.com>
2015-03-03 23:45:33 +0100

    Initial

---
+++ ApBox/ButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ApBox
+{
+    public static class ButtonBehavior
+    {
+        public static readonly DependencyProperty GeometryProperty = DependencyProperty.RegisterAttached(
+            "Geometry",
+            typeof (Geometry), 
+            typeof (ButtonBehavior),
+            new PropertyMetadata(default(Geometry)));
+
+        public static void SetGeometry(DependencyObject element, Geometry value)
+        {
+            element.SetValue(GeometryProperty, value);
+        }
+
+        public static Geometry GetGeometry(DependencyObject element)
+        {
+            return (Geometry) element.GetValue(GeometryProperty);
+        }
+    }
+}
